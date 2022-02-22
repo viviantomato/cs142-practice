89a4688141e018158a89413820f17a4578374236
viviantomato <vivianyw924@gmail.com>
2022-02-22 12:18:58 -0800

    able to print index from line

--- hw7/Personality.java
+++ hw7/Personality.java
@@ -12,25 +12,51 @@ public class Personality {
    while (input.hasNextLine()) {
       String name = input.nextLine();
       String line = input.nextLine();
-      System.out.println(name);
+      System.out.println(name);//output1
       System.out.println(line);
-      //convert String into int[]
-      //*int[] countAB = computeCountAB(line);
-      //use counter or cumulative sum to test condition; 
+      //convert String into int[], use counter for results
+      
+      //*int[] countResults = countResults(line);
+    
       //*System.out.println(Arrays.toString(countAB)); 
    }  
    }
-//    public static computeCountAB(String line ) {
-//       int[] countAB = new int [line.length()];//String needs ()
-//       //String travasal
-//       for (int i = 0; i < line.length(); i++) {
-//          //do sth with line.charAt(i), all initial to zero in array
-//          char next = linecharAt(i);
-//          //if (line.charAT(i) is a 1) { // ' ' single quote for char
-//             //scores[i] = number;
-//          }
+    public static int[] countResults(String line) {
+       int[] countAResults = new int [4];//String needs ()
+       //int[] countBResults = new int [4];
+         for (int i = 0; i < 4; i++) { //loop 0-3 for new array
+       
+         for (int j = 0; j < 70 / 7; j++) { //loop 0-9 for index in line
+            if (i == 0) {
+                int index = j * 7;  
+                char next = line.charAt(index); //read char from index position
+                test (next); // test char for a, b, - and increase counter
+            }
+            
+            else {
+
+                int index = j * 7 + 2 * i - 1;
+                int index2 = j * 7 + 2 * i;
+                char next = line.charAt(index);
+                char next = line.charAt(index2);
+            }
+            
+         }
+        }        
+
+                 
+          //char next = line.charAt(i);
+          
+       }
+//           if (next.toLowerCase == 'a') {// ' ' single quote for char
+//           else if (next.toLowerCase == 'b') 
+//           else (next.toLowerCase == '-') 
+             
+          }
 //          
 //       }
 //       return countAB;
-//    }
+    }
+}
+}
 }
