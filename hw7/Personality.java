ebd871210f071fee68dbc41864a8920ee25f613a
viviantomato <vivianyw924@gmail.com>
2022-02-22 16:35:25 -0800

    able to cal & print Array{countAResults}

--- hw7/Personality.java
+++ hw7/Personality.java
@@ -6,57 +6,50 @@ import java.io.*;
 //check coding style
 
 public class Personality {
-   public static void main(String[] args) throws FileNotFoundException {
-   //String 70 chars - turn into two array, for line based file
-   Scanner input = new Scanner (new File ("personality.txt"));
-   while (input.hasNextLine()) {
-      String name = input.nextLine();
-      String line = input.nextLine();
-      System.out.println(name);//output1
-      System.out.println(line);
-      //convert String into int[], use counter for results
-      
-      //*int[] countResults = countResults(line);
-    
-      //*System.out.println(Arrays.toString(countAB)); 
-   }  
-   }
-    public static int[] countResults(String line) {
-       int[] countAResults = new int [4];//String needs ()
-       //int[] countBResults = new int [4];
-         for (int i = 0; i < 4; i++) { //loop 0-3 for new array
-       
-         for (int j = 0; j < 70 / 7; j++) { //loop 0-9 for index in line
-            if (i == 0) {
-                int index = j * 7;  
-                char next = line.charAt(index); //read char from index position
-                test (next); // test char for a, b, - and increase counter
-            }
-            
-            else {
-
-                int index = j * 7 + 2 * i - 1;
-                int index2 = j * 7 + 2 * i;
-                char next = line.charAt(index);
-                char next = line.charAt(index2);
-            }
-            
-         }
-        }        
-
-                 
-          //char next = line.charAt(i);
-          
-       }
-//           if (next.toLowerCase == 'a') {// ' ' single quote for char
-//           else if (next.toLowerCase == 'b') 
-//           else (next.toLowerCase == '-') 
-             
-          }
-//          
-//       }
-//       return countAB;
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner (new File ("personality.txt"));
+        while (input.hasNextLine()) {
+            String name = input.nextLine();
+            String line = input.nextLine();
+            System.out.println(name); //output1
+            System.out.println(line);
+            int[] countAResults = countResults(line);            
+            System.out.println(Arrays.toString(countAResults));
+            //System.out.println(Arrays.toString(countBResults));
+        }
     }
-}
-}
+    
+    public static int[] countResults(String line) {
+        int[] countAResults = new int [4]; 
+        int[] countBResults = new int [4];
+        int[] dashCount = new int [4];
+        for (int i = 0; i < 4; i++) { // loop 0-3 for new array
+            for (int j = 0; j < 70 / 7; j++) { //loop 0-9 for index in line
+                if (i == 0) {
+                    int index = j * 7;
+                    char next = line.charAt(index); // read char from index position
+                    test (next, i, countAResults, countBResults, dashCount); // test char for a, b, - and increase counter
+                } else {                   
+                    int index = j * 7 + 2 * i - 1;
+                    char next = line.charAt(index);
+                    test (next, i, countAResults, countBResults, dashCount);
+                    int index2 = j * 7 + 2 * i;
+                    char next2 = line.charAt(index2);
+                    test (next2, i, countAResults, countBResults, dashCount);
+                }
+            }
+        }
+        return countAResults; //how to return three array
+ 
+    }
+    public static void test(char next, int i, int[] countAResults, int[] countBResults, int[] dashCount) {
+         
+         if (next == 'a' || next == 'A') {   // ' ' single quote for char
+         countAResults[i] ++;
+         } else if (next == 'b' || next == 'B') {
+          countBResults[i] ++;
+         } else {
+          dashCount[i] ++;
+         }    
+    }
 }
