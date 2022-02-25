324846f4a3cae0458191254a056a7b71c51301fe
viviantomato <vivianyw924@gmail.com>
2022-02-25 14:25:05 -0800

    add constant, format, compared output

--- hw7/Personality.java
+++ hw7/Personality.java
@@ -5,43 +5,40 @@ import java.io.*;
 //add intro; swap constant for four;
 //check coding style
 
-public class Personality {   
+public class Personality {
+    public static final int arraySize = 4;
     //main method reads from original data, capturing name and line
-    public static void main(String[] args) throws FileNotFoundException {            
+    public static void main(String[] args) throws FileNotFoundException {
         //print program intro
-        System.out.print("This program processes a file of answers to the\nKeirsey Temperament Sorter. It converts the \nvarious A and B answers for each person into \na sequence of B-percentages and then into a \nfour-letter personality type.");
+        System.out.print("This program processes a file of answers to the\nKeirsey Temperament Sorter.  It converts the \nvarious A and B answers for each person into \na sequence of B-percentages and then into a \nfour-letter personality type.\n");
+        System.out.println();
         //read from input
         System.out.print("input file name? ");
         Scanner console = new Scanner(System.in);
         String inputFile = console.next();
-        
-        //read from output
-        System.out.print("output file name? ");
-        
         Scanner input = new Scanner (new File (inputFile));
+        //create output
+        System.out.print("output file name? ");
+        String outputFile = console.next();
+        PrintStream output = new PrintStream(new File (outputFile));
         while (input.hasNextLine()) {
             String name = input.nextLine();
             String line = input.nextLine();
-            System.out.print(name + ": ");
+            output.print(name + ": ");
             int[] countBPercent = bPecent(line);
-            System.out.print(Arrays.toString(countBPercent));
-            System.out.print(" = " );
+            output.print(Arrays.toString(countBPercent));
+            output.print(" = " );
             String[] type = typeTest (countBPercent);
-            for (int k = 0; k < type.length; k++) {
-                System.out.print(type[k]);
-            }
-            System.out.println();
-            
-            
+            print(output, type);
         }
     }
     
     //this method reads from line and pinpoint the index of the character to test in four catagories, return array of B percentage
     public static int[] bPecent(String line) {
-        int[] countANumber = new int [4];
-        int[] countBNumber = new int [4];
-        int[] countBPercent = new int [4];
-        for (int i = 0; i < 4; i++) {  // loop 0-3 for new array
+        int[] countANumber = new int [arraySize];
+        int[] countBNumber = new int [arraySize];
+        int[] countBPercent = new int [arraySize];
+        for (int i = 0; i < arraySize; i++) {  // loop 0-3 for new array
             for (int j = 0; j < 70 / 7; j++) { // loop 0-9 for index in line
                 if (i == 0) {
                     char next = line.charAt(j * 7); // read char from calculated index position
@@ -67,30 +64,30 @@ public class Personality {
         }
     }
     
+    //this method generates letter stands for type based on countBPercent
     public static String[] typeTest (int[] countBPercent) {
-        String[] type = new String [4];
-        
+        String[] type = new String [arraySize];
         if (countBPercent[0] > 50) {
             type[0] = "I";
         } else if (countBPercent[0] < 50) {
             type[0] = "E";
         } else {
             type[0] = "X";
-        }       
+        }
         if (countBPercent[1] > 50) {
             type[1] = "N";
         } else if (countBPercent[1] < 50) {
             type[1] = "S";
         } else {
             type[1] = "X";
-        }       
+        }
         if (countBPercent[2] > 50) {
             type[2] = "F";
         } else if (countBPercent[2] < 50) {
             type[2] = "T";
         } else {
             type[2] = "X";
-        }                
+        }
         if (countBPercent[3] > 50) {
             type[3] = "P";
         } else if (countBPercent[3] < 50) {
@@ -99,5 +96,13 @@ public class Personality {
             type[3] = "X";
         }
         return type;
+    }
+    
+    //print each letter from String[] type  
+    public static void print(PrintStream output, String[] type) {
+        for (int k = 0; k < arraySize; k++) {
+            output.print(type[k]);
+        }
+        output.println();
     }
 }
